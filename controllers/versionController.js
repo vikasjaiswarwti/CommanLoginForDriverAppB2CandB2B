1585eeab5b96dcce984dba5d20665cb271609f12
vikasjaiswarwti <167772672+vikasjaiswarwti@users.noreply.github.com>
2026-09-01 11:36:08 +0530

    version updated for driver app - 1.0.0 ->1.0.8

--- controllers/versionController.js
+++ controllers/versionController.js
@@ -8,7 +8,7 @@ const checkversionifrequiredupdate = async (req, res) => {
         .json({ required: false, message: "Version is required" });
     }
 
-    const isUpToDate = isVersionUpToDate(version, "1.0.0");
+    const isUpToDate = isVersionUpToDate(version, "1.0.8");
 
     if (isUpToDate) {
       return res
